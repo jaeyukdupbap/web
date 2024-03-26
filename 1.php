c63995d95f43dd13f5a9f8bfe944dae1e666918b
jaeyukdupbap <shvltld0@gmail.com>
2024-03-26 22:51:50 +0900

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 100;
+    $b = 50;
+    $c = $a + $b;
+    echo "\$c = $c<br>"; //출력결과 $c = 150
+
+    // 문자열 같은 경우에는
+    $string = "1234"
+    $string2 = "5678"
+    $string3 = $string. $string2;
+    echo "\$string = $string<br>"; //출력결과 $string = 12345678
+?>
