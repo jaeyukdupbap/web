128f1a8d6cd2656ae65e3321533e094dbd9e2996
jaeyukdupbap <shvltld0@gmail.com>
2024-03-28 14:54:22 +0900

    update_2

--- 1.php
+++ 1.php
@@ -1,4 +1,5 @@
 <?php
+    // 기본 연산
     $a = 100;
     $b = 50;
     $c = $a + $b;
@@ -9,4 +10,9 @@
     $string2 = "5678"
     $string3 = $string. $string2;
     echo "\$string = $string<br>"; //출력결과 $string = 12345678
+    
+    // img 태그 사용
+    echo "<img src='이미지'></img>";
 ?>
+<p>이런 식으로도 가능합니다.</p>
+<p>a의 값은 = <?=$a></p>
